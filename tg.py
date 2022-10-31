6de2c03c99fe4b6bc5078fb3c53297c37f0abb45
gigianni <103031839+gigianni@users.noreply.github.com>
2022-10-31 15:49:12 +0100

    Update tg.py v1.3
    Added track buttons (currently in beta), as you can see by trying the bot at https://t.me/gtt_tgbot a reply keyboard has been added!
    Its buttons can be a shortcut to stop or line messages but also to tracks, a track is a list of consecutive stops made by a route here an example
    *624 L15 566 will give me every 15's (it's a tram) trip with his departure time from stop 624, it's arrival time at stop 566 and every departing bus from that stop.
    In this way a user can look up for coincidences.
    
    Here an example of the response https://raw.githubusercontent.com/gigianni/GTTtg/main/img/tg_track.png

--- tg.py
+++ tg.py
@@ -1,14 +1,10 @@
-import logging
-
-import telegram
-
 import main as m
+import logging
+import telegram
 import datetime as dt
-import io
 import threading
-
-from telegram import Update, ForceReply, ParseMode, InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, CallbackQueryHandler
+from telegram import Update, ParseMode, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, CallbackQueryHandler, PicklePersistence
 
 f = open("../tg.txt", "r")
 TOKEN = f.read().rstrip()
@@ -22,16 +18,80 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-# Define a few command handlers. These usually take the two arguments update and
-# context.2
 def start(update: Update, context: CallbackContext) -> None:
 	"""Send a message when the command /start is issued."""
-	user = update.effective_user
-	update.message.reply_markdown_v2(
-		fr'Hi {user.mention_markdown_v2()}\!',
-		reply_markup=ForceReply(selective=True),
-	)
+	if 'keyboard' in context.user_data:
+		update.message.reply_text("Hi!", reply_markup=ReplyKeyboardMarkup(context.user_data['keyboard'], resize_keyboard=True))
+	else:
+		update.message.reply_text("Hi!", reply_markup=ReplyKeyboardMarkup([["+"]], resize_keyboard=True))
 
+
+def routeNormalizer(text):
+	"""Return route_id format from L<route_short_name> format, route_id could not be present in RT class"""
+	return text[1:].upper().replace(" ", "") + 'U'
+
+
+def add_command(update: Update, context: CallbackContext) -> None:
+	"""Send a message when the command /add is issued."""
+	if 'add_mode' not in context.user_data or context.user_data['add_mode'] == 0:
+		context.user_data['add_mode'] = 1
+		update.message.reply_text('Rispondi con la fermata di partenza')
+	else:
+		if context.user_data['add_mode'] == 1:
+			context.user_data["add_content"] = update.message.text
+		else:
+			context.user_data["add_content"] += " "+update.message.text
+		context.user_data['add_mode'] += 1
+		sendTrackData(update.message.chat_id, context.user_data["add_content"].split(' '))
+		keyboard = [[InlineKeyboardButton("Salva", callback_data='+'), InlineKeyboardButton("Annulla", callback_data='-')]]
+		reply_markup = InlineKeyboardMarkup(keyboard)
+		update.message.reply_text("Per fare una tratta seleziona una linea inviando L<codice linea>.\nPer aggiungere un'altra fermata manda il codice.", reply_markup=reply_markup)
+
+
+def save_keyboard(chat_id, context):
+	"""Saves the keyboard."""
+	if context.user_data['add_mode'] > 0:
+		keyboard = context.user_data.setdefault("keyboard", [["+"]])
+		keyboard[len(keyboard)//3].insert(0, "*"+context.user_data["add_content"])
+		context.user_data['add_mode'] = 0
+		context.user_data['keyboard'] = keyboard
+		upd.bot.sendMessage(chat_id=chat_id, text="Done.", reply_markup=ReplyKeyboardMarkup(keyboard, resize_keyboard=True))
+
+
+def discard_keyboard(chat_id, context):
+	"""Saves the keyboard."""
+	context.user_data['add_mode'] = 0
+	upd.bot.sendMessage(chat_id=chat_id, text="Done.")
+
+
+def sendTrackData(chat_id, data):
+	msg = ""
+
+	for i in range(len(data)):
+		if data[i][0].lower() != 'l':
+
+			if i+2 < len(data) and data[i+1][0].lower() == 'l':
+				route_id = routeNormalizer(data[i+1])
+				data[i+1] = 'L'+route_id
+				stop = m.RT.get_stop_from_stopcode(data[i])
+				next_stop = m.RT.get_stop_from_stopcode(data[i+2])
+				if stop is not None and next_stop is not None:
+					msg += f"Linea {route_id[:-1]} tratta:\n\t<b>{stop['stop_name'][8:]}"
+					msg += f"\t\t-->\t\t{next_stop['stop_name'][8:]}</b>\n\n"
+					stop_id = m.RT.stopcodes[data[i]]
+					if route_id in m.RT.stops[stop_id]["stop_times"]:
+						for key, el in m.RT.stops[stop_id]["stop_times"][route_id]["times"].items():
+							arrival = m.RT.trips[key.split('-')[0]]['stop_times'][m.RT.stopcodes[data[i+2]]]
+							msg += f"\t{dt.datetime.fromtimestamp(el['timestamp']).strftime('%H:%M')}" \
+									f"\t\t\t±{round(el['std_dev'])} sec\t\t-->\t\t" \
+									f"\t\t{dt.datetime.fromtimestamp(arrival['timestamp']).strftime('%H:%M')}" \
+									f"\t\t\t±{round(arrival['std_dev'])} sec\n\n"
+					else:
+						msg += f"\t\tNessun arrivo per la linea {data[i+1][:-1]}\n"
+			else:
+				msg += getStopData(data[i])[0]
+
+	upd.bot.sendMessage(chat_id=chat_id, text=msg, parse_mode=ParseMode.HTML)
 
 def help_command(update: Update, context: CallbackContext) -> None:
 	"""Send a message when the command /help is issued."""
@@ -51,21 +111,56 @@ def json_command(update: Update, context: CallbackContext) -> None:
 	f.close()
 	update.message.reply_text('Done')
 
+def getStopData(text):
+	keyboard = []
+	s = m.getStopRT(text)
+	reply_markup = None
+	location = None
+	if s[0] == -1:
+		msg = "Fermata non trovata"
+	elif len(s[1]["stop_times"]) == 0:
+		msg = "Nessun arrivo previsto"
+		location = telegram.Location(longitude=s[1]["stop_lon"], latitude=s[1]["stop_lat"])
+	else:
+		msg = f"<b>{s[1]['stop_name']}</b>\n{s[1]['stop_desc']}\n"
+		i = 0
+		for route_id, route_times in s[1]["stop_times"].items():
+			msg += f"\n<b>{route_times['route_short_name']}</b>\n"
+			if not i % 3:
+				keyboard.append([])
+			keyboard[i // 3].append(
+				InlineKeyboardButton("Linea " + route_times["route_short_name"],
+									 callback_data=route_id + "_" + next(iter(route_times["times"])).split('-')[0]))
+			i += 1
+			times = list(route_times["times"].values())
+			# print in order of timestamp
+			while len(times) != 0:
+				min = 0
+				j = 1
+				while j < len(times):
+					if times[j]["timestamp"] < times[min]["timestamp"]:
+						min = j
+					j += 1
 
-def sendRouteData(chat_id, route_id, trip_id='-1'):
+				msg += f"\t\t{dt.datetime.fromtimestamp(times[min]['timestamp']).strftime('%H:%M')}" \
+					   f"\t±{round(times[min]['std_dev'])} sec\n"
+				times.pop(min)
+		reply_markup = InlineKeyboardMarkup(keyboard)
+		location = telegram.Location(longitude=s[1]["stop_lon"], latitude=s[1]["stop_lat"])
+	return msg,location,reply_markup
+
+def getRouteData(route_id, trip_id='-1'):
 	keyboard = []
 	s = m.getRouteRT(route_id)
+	location = None
 	if s[0] == -1:
 		msg = "Linea non trovata"
 	else:
-		if trip_id == '-1':
-			for id, trip in s[1].items():
-				if trip["stop_times_count"] > 0:
-					trip_id = id
-					break
-		if trip_id == '-1':
+		if len(s[1]) == 0:
 			msg = "Nessun passaggio trovato"
 		else:
+			if trip_id == '-1':
+				trip_id = next(iter(s[1]))
 			msg = f"<b>Linea {s[1][trip_id]['route_short_name']}</b>\n"
 			if s[1][trip_id]["limited"] == 1:
 				msg += "<b>LIMITATO</b> vedi @gttavvisi\n"
@@ -77,91 +172,85 @@ def sendRouteData(chat_id, route_id, trip_id='-1'):
 						   f"\t\t{dt.datetime.fromtimestamp(stop_time['timestamp']).strftime('%H:%M')}" \
 						   f"\t\t\t±{round(stop_time['std_dev'])} sec\n"
 
-
 			i = 1
 			aDir = bDir = ""
 			for id, trip in s[1].items():
-				if trip["stop_times_count"] > 0:
-					if trip["direction"] == 0:
-						txt = "A - "
-						aDir = trip["headsign"]
-					else:
-						txt = "B - "
-						bDir = trip["headsign"]
-					txt += m.RT.stops[next(iter(trip["stop_times"]))]["stop_name"][8:]
-					if not (i - 1) % 2:
-						keyboard.append([])
-					keyboard[(i - 1) // 2].append(InlineKeyboardButton(txt, callback_data=route_id + '_' + id))
-					i += 1
+				if trip["direction"] == 0:
+					txt = "A - "
+					aDir = trip["headsign"]
+				else:
+					txt = "B - "
+					bDir = trip["headsign"]
+				txt += m.RT.stops[next(iter(trip["stop_times"]))]["stop_name"][8:]
+				if not (i - 1) % 2:
+					keyboard.append([])
+				keyboard[(i - 1) // 2].append(InlineKeyboardButton(txt, callback_data=route_id + '_' + id))
+				i += 1
 			if aDir != "" or bDir != "":
 				msg += "\nAltri mezzi nel formato <i>Direzione - Prossima fermata</i>, con direzione:\n"
 				msg += f"A: {aDir}\nB: {bDir}"
 
 			position = s[1][trip_id]['position']
 			if position["timestamp"] != 0:
-				upd.bot.sendLocation(chat_id=chat_id, location=telegram.Location(longitude=position["longitude"],
-								latitude=position["latitude"], live_period=10, heading=position["bearing"]))
+				location = telegram.Location(longitude=position["longitude"],
+									latitude=position["latitude"], live_period=10, heading=position["bearing"])
 
 	reply_markup = InlineKeyboardMarkup(keyboard)
-	upd.bot.sendMessage(chat_id=chat_id, text=msg, parse_mode=ParseMode.HTML, reply_markup=reply_markup)
+	return msg, location, reply_markup
 
 
 def texthandler(update: Update, context: CallbackContext) -> None:
-	if update.message.text[0].lower() == 'l':
-		sendRouteData(update.message.chat_id, update.message.text[1:] + 'U')
+	init = update.message.text[0].lower()
+	if init == "+" or 'add_mode' in context.user_data and context.user_data['add_mode'] != 0:
+		add_command(update, context)
+	elif init == '*':
+		# button pressed
+		sendTrackData(update.message.chat_id, update.message.text[1:].split(' '))
 	else:
-		keyboard = []
-		s = m.getStopRT(update.message.text)
-		if s[0] == -1:
-			msg = "Fermata non trovata"
-		elif len(s[1]["stop_times"]) == 0:
-			msg = "Nessun arrivo previsto"
+		if init == 'l':
+			# route type
+			msg, location, reply_markup = getRouteData(routeNormalizer(update.message.text))
+			send_location_data(update.message.chat_id, context, msg, location, reply_markup)
 		else:
-			msg = f"<b>{s[1]['stop_name']}</b>\n{s[1]['stop_desc']}\n"
-			i = 0
-			for route_id, route_times in s[1]["stop_times"].items():
-				msg += f"\n<b>{route_times['route_short_name']}</b>\n"
-				if not i % 3:
-					keyboard.append([])
-				keyboard[i//3].append(
-					InlineKeyboardButton("Linea " + route_times["route_short_name"],
-						callback_data=route_id + "_" + next(iter(route_times["times"])).split('-')[0]))
-				i += 1
-				times = list(route_times["times"].values())
-				# print in order of timestamp
-				while len(times) != 0:
-					min = 0
-					j = 1
-					while j < len(times):
-						if times[j]["timestamp"] < times[min]["timestamp"]:
-							min = j
-						j += 1
+			# stopcode
+			msg, location, reply_markup = getStopData(update.message.text)
+			send_location_data(update.message.chat_id, context, msg, location, reply_markup)
 
-					msg += f"\t\t{dt.datetime.fromtimestamp(times[min]['timestamp']).strftime('%H:%M')}" \
-						   f"\t±{round(times[min]['std_dev'])} sec\n"
-					times.pop(min)
-		reply_markup = InlineKeyboardMarkup(keyboard)
-		upd.bot.sendLocation(chat_id=update.message.chat_id, location=telegram.Location(longitude=s[1]["stop_lon"],
-																		 latitude=s[1]["stop_lat"]))
-		update.message.reply_text(msg, parse_mode=ParseMode.HTML, reply_markup=reply_markup)
+
+def send_location_data(chat_id, context, msg, location, reply_markup):
+	if location is not None:
+		if 'keyboard' in context.user_data:
+			upd.bot.sendLocation(chat_id=chat_id, location=location,
+								 reply_markup=ReplyKeyboardMarkup(context.user_data['keyboard'], resize_keyboard=True))
+		else:
+			upd.bot.sendLocation(chat_id=chat_id, location=location)
+
+	upd.bot.sendMessage(chat_id=chat_id, text=msg, parse_mode=ParseMode.HTML, reply_markup=reply_markup)
 
 
 def button(update: Update, context: CallbackContext) -> None:
 	query = update.callback_query
 	query.answer()
-	routeId, tripId = query.data.split('_')[0], query.data.split('_')[1]
-
-	sendRouteData(query.message.chat_id, routeId, tripId)
+	if query.data[0] == '+':
+		save_keyboard(query.message.chat_id, context)
+	elif query.data[0] == '-':
+		discard_keyboard(query.message.chat_id, context)
+	else:
+		routeId, tripId = query.data.split('_')[0], query.data.split('_')[1]
+		msg, location, reply_markup = getRouteData(routeId, tripId)
+		send_location_data(query.message.chat_id, context, msg, location, reply_markup)
 
 
 upd = None
 def main() -> None:
-	updater = Updater(TOKEN)
+	persistence = PicklePersistence(filename="GTTtg_bot_persistence", store_user_data=True)
+	updater = Updater(TOKEN, use_context=True, persistence=persistence)
 	global upd
 	upd = updater
 	dispatcher = updater.dispatcher
 
 	dispatcher.add_handler(CommandHandler("start", start))
+	dispatcher.add_handler(CommandHandler("add", add_command))
 	dispatcher.add_handler(CommandHandler("help", help_command))
 	dispatcher.add_handler(CommandHandler("logs", logs_command, Filters.chat(chat_id=84266954)))
 	dispatcher.add_handler(CommandHandler("json", json_command, Filters.chat(chat_id=84266954)))
